cdc491cc459d2c3f7e9aeba2dbdf53e63659087e
Sahat Yalkabov <sakhat@gmail.com>
2014-03-01 14:19:39 -0500

    Revised User model, moved passport strategy into app.js

--- app.js
+++ app.js
@@ -3,25 +3,77 @@ var flash = require('express-flash');
 var path = require('path');
 var mongoose = require('mongoose');
 var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
 var expressValidator = require('express-validator');
 var less = require('less-middleware');
 
-
 var homeController = require('./controllers/home');
-var userController = require('./controllers/user');
-var contactController = require('./controllers/contact');
 
-var secrets = require('./config/secrets');
-var passportConf = require('./config/passport');
+var config = require('./config');
+
+var User = mongoose.model('User', new mongoose.Schema({
+  facebookId: String,
+  accessToken: String,
+  email: String,
+  firstName: String,
+  lastName: String,
+  profileUrl: String,
+  gender: String,
+  location: String,
+  birthday: String,
+  hereFor: String,
+  picture: String,
+  likes: Number,
+  dislikes: Number
+}));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new FacebookStrategy(config.facebook, function(req, accessToken, refreshToken, profile, done) {
+  User.findOne({ facebookId: profile.id }, function(err, existingUser) {
+    if (existingUser) return done(null, existingUser);
+    console.log(profile);
+    var user = new User({
+      facebookId: profile.id,
+      accessToken: accessToken,
+      email: profile._json.email,
+      profileUrl: profile.profileUrl,
+      firstName: profile.name.givenName,
+      lastName: profile.name.familyName,
+      gender: profile._json.gender,
+      location: (profile._json.location) ? profile._json.location.name : '',
+      birthday: profile._json.birthday,
+      picture: 'https://graph.facebook.com/' + profile.id + '/picture?type=large'
+    });
+    user.save(function(err) {
+      done(err, user);
+    });
+  });
+}));
+
+var isAuthenticated = function(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.redirect('/login');
+};
+
+var isAuthorized = function(req, res, next) {
+  var provider = req.path.split('/').slice(-1)[0];
+  if (_.findWhere(req.user.tokens, { kind: provider })) next();
+  else res.redirect('/auth/' + provider);
+};
+
 
 var app = express();
 
-
-mongoose.connect(secrets.db);
-mongoose.connection.on('error', function() {
-  console.error('✗ MongoDB Connection Error. Please make sure MongoDB is running.');
-});
-
+mongoose.connect('localhost');
 
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
@@ -50,23 +102,69 @@ app.use(express.errorHandler());
  */
 
 app.get('/', homeController.index);
-app.get('/login', userController.getLogin);
-app.post('/login', userController.postLogin);
-app.get('/logout', userController.logout);
-app.get('/signup', userController.getSignup);
-app.post('/signup', userController.postSignup);
-app.get('/contact', contactController.getContact);
-app.post('/contact', contactController.postContact);
-app.get('/account', passportConf.isAuthenticated, userController.getAccount);
-app.post('/account/profile', passportConf.isAuthenticated, userController.postUpdateProfile);
+app.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+/**
+ * GET /signup
+ * Signup page.
+ */
+
+app.get('/signup', function(req, res) {
+  if (req.user) return res.redirect('/');
+  res.render('signup', {
+    title: 'Create Account'
+  });
+});
+
+/**
+ * POST /signup
+ * Create a new local account.
+ * @param email
+ * @param password
+ */
+
+app.post('/signup', function(req, res, next) {
+  req.assert('email', 'Email is not valid').isEmail();
+  req.assert('password', 'Password must be at least 4 characters long').len(4);
+  req.assert('confirmPassword', 'Passwords do not match').equals(req.body.password);
+
+  var errors = req.validationErrors();
+
+  if (errors) {
+    req.flash('errors', errors);
+    return res.redirect('/signup');
+  }
+
+  var user = new User({
+    email: req.body.email,
+    password: req.body.password
+  });
+
+  user.save(function(err) {
+    if (err) {
+      if (err.code === 11000) {
+        req.flash('errors', { msg: 'User with that email already exists.' });
+      }
+      return res.redirect('/signup');
+    }
+    req.logIn(user, function(err) {
+      if (err) return next(err);
+      res.redirect('/');
+    });
+  });
+});
+
 
 /**
  * OAuth routes for sign-in.
  */
 
-app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email', 'user_location'] }));
+app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email', 'user_location', 'user_birthday'] }));
 app.get('/auth/facebook/callback', passport.authenticate('facebook', { successRedirect: '/', failureRedirect: '/login' }));
 
 app.listen(app.get('port'), function() {
-  console.log("✔ Express server listening on port ", app.get('port'));
+  console.log("Express server listening on port ", app.get('port'));
 });
